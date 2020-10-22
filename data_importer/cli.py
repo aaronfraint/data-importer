a2997737125909e4a87d810f234029b6aaef4525
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-10-22 10:21:34 -0400

    wire up OSM command

--- data_importer/cli.py
+++ data_importer/cli.py
@@ -3,9 +3,15 @@ import click
 import os
 from dotenv import load_dotenv, find_dotenv
 
+import postgis_helpers as pGIS
+
+from .openstreetmap import import_osm_for_dvrpc_region
 
 load_dotenv(find_dotenv(usecwd=True))
 DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+
+credentials = pGIS.configurations()
 
 
 @click.group()
@@ -15,19 +21,20 @@ def main():
 
 
 @click.command()
-def test():
-    print(DB_NAME)
-    # print(os.getcwd())
+@click.option(
+    "--networktype", "-n",
+    help="Network type to download (see osmnx docs)",
+    default="drive",
+)
+def osm(networktype):
+    """Download OpenStreetMap data for the DVRPC region.
+       Defaults to the 'drive' network
+    """
+    db = pGIS.PostgreSQL(DB_NAME, **credentials[DB_HOST])
+    import_osm_for_dvrpc_region(db, network_type=networktype)
 
 
-@click.command()
-def import_data():
-    """Import RideScore data into the SQL database"""
-    ridescore_inputs = GDRIVE_ROOT / "inputs"
-    import_shapefiles(ridescore_inputs, db)
-
-
-all_commands = [test, import_data]
+all_commands = [osm]
 
 for cmd in all_commands:
     main.add_command(cmd)
